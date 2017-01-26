63395189e1ff6d369c6dc3858e9755c2ef673d44
Lilian Kasem <liliankasem@gmail.com>
2017-01-26 16:56:02 +0000

    init commit of report

---
+++ EmbedSample/Controllers/DashboardController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.PowerBI.Api.V1;
+using Microsoft.PowerBI.Security;
+using Microsoft.Rest;
+using paas_demo.Models;
+using Microsoft.PowerBI.Api.V1.Models;
+using System.Threading;
+using System.IO;
+
+namespace paas_demo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly string workspaceCollection;
+        private readonly string workspaceId;
+        private readonly string accessKey;
+        private readonly string apiUrl;
+
+        public DashboardController()
+        {
+            this.workspaceCollection = ConfigurationManager.AppSettings["powerbi:WorkspaceCollection"];
+            this.workspaceId = ConfigurationManager.AppSettings["powerbi:WorkspaceId"];
+            this.accessKey = ConfigurationManager.AppSettings["powerbi:AccessKey"];
+            this.apiUrl = ConfigurationManager.AppSettings["powerbi:ApiUrl"];
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Reports()
+        {
+            using (var client = this.CreatePowerBIClient())
+            {
+                var reportsResponse = client.Reports.GetReports(this.workspaceCollection, this.workspaceId);
+
+                var viewModel = new ReportsViewModel
+                {
+                    Reports = reportsResponse.Value.ToList()
+                };
+
+                return PartialView(viewModel);
+            }
+        }
+
+        public async Task<ActionResult> Report(string reportId)
+        {
+            using (var client = this.CreatePowerBIClient())
+            {
+                var reportsResponse = await client.Reports.GetReportsAsync(this.workspaceCollection, this.workspaceId);
+                var report = reportsResponse.Value.FirstOrDefault(r => r.Id == reportId);
+                var embedToken = PowerBIToken.CreateReportEmbedToken(this.workspaceCollection, this.workspaceId, report.Id);
+
+                var viewModel = new ReportViewModel
+                {
+                    Report = report,
+                    AccessToken = embedToken.Generate(this.accessKey)
+                };
+
+                return View(viewModel);
+            }
+        }
+
+        public ActionResult Import()
+        {
+
+            return View();
+        }
+
+        public async Task<Import> ImportPBIX(string datasetName, string filePath)
+        {
+            using (var fileStream = System.IO.File.OpenRead(filePath.Trim('"')))
+            {
+                using (var client = this.CreatePowerBIClient())
+                {
+                    // Set request timeout to support uploading large PBIX files
+                  //  client.HttpClient.Timeout = TimeSpan.FromMinutes(60);
+                  //  client.HttpClient.DefaultRequestHeaders.Add("ActivityId", Guid.NewGuid().ToString());
+
+                    // Import PBIX file from the file stream
+                    var import = await client.Imports.PostImportWithFileAsync(workspaceCollection, workspaceId, fileStream, datasetName);
+
+                    // Example of polling the import to check when the import has succeeded.
+                    while (import.ImportState != "Succeeded" && import.ImportState != "Failed")
+                    {
+                        import = await client.Imports.GetImportByIdAsync(workspaceCollection, workspaceId, import.Id);
+                        Console.WriteLine("Checking import state... {0}", import.ImportState);                        
+                        Thread.Sleep(1000);
+                    }
+
+                    return import;
+                }
+            }
+        }
+
+
+        private IPowerBIClient CreatePowerBIClient()
+        {
+            var credentials = new TokenCredentials(accessKey, "AppKey");
+            var client = new PowerBIClient(credentials)
+            {
+                BaseUri = new Uri(apiUrl)
+            };
+ 
+            return client;
+        }
+
+    }
+}
